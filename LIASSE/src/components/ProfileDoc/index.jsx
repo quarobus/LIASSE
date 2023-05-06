d4a2b917a6257e6e082925f239e1943aa8dfb143
Fadel El <fadaaaa2019@gmail.com>
2023-05-06 21:06:52 +0100

    Fadel Update

--- LIASSE/src/components/ProfileDoc/index.jsx
+++ LIASSE/src/components/ProfileDoc/index.jsx
@@ -11,6 +11,8 @@ function ProfileDoc() {
             <div class="A-profil">
                 <div class="profilPic-profil">
                     <img class="img-profil" src={profil} height="120px" width="120px"/>
+                    <input className="Post-input" type="file" id="image" name="image" />
+                    <label class="ImportImg-profil" for="image">Upload Picture</label>
                     <h2>Full Name</h2>
                     <center><hr class="hr-profil"/></center>
                     <table class="T-profil linkTable-profil">
@@ -36,45 +38,49 @@ function ProfileDoc() {
                 </div>
             </div>
 
-            <div class="B-profil">
-                <table class="T-profil ProfileDataTable-profil">
-                    <tr>
-                      <td>Nom</td>
-                      <td>Rachid</td>
+                    <div class="B-profil">
+                        <table class="T-profil ProfileDataTable-profil">
+                            <tr>
+                        <td><label for="nom">Nom</label></td>
+                        <td><input type="text" id="nom" name="nom" value="Kamal"/></td>
                     </tr>
                     <tr>
-                      <td>Prénom</td>
-                      <td>Rachid</td>
+                        <td><label for="prenom">Prénom</label></td>
+                        <td><input type="text" id="prenom" name="prenom" value="Rami"/></td>
                     </tr>
                     <tr>
-                      <td>Spécialité</td>
-                      <td>Informatique</td>
+                        <td><label for="specialite">Spécialité</label></td>
+                        <td><input type="text" id="specialite" name="specialite" value="Info"/></td>
                     </tr>
                     <tr>
-                      <td>Thématiques de travail</td>
-                      <td>Game Dev</td>
+                        <td><label for="thematiques">Thématiques de travail</label></td>
+                        <td><input type="text" id="thematiques" name="thematiques" value="Game Dev"/></td>
                     </tr>
                     <tr>
-                      <td>Le sujects de thèse</td>
-                      <td>ML & Game Dev</td>
+                        <td><label for="sujets_these">Le suject de thèse</label></td>
+                        <td><input type="text" id="sujets_these" name="sujets_these" value="link"/></td>
                     </tr>
                     <tr>
-                      <td>Les articles réalisés</td>
-                      <td>link</td>
+                        <td><label for="articles_realises">Les articles réalisés</label></td>
+                        <td><input type="text" id="articles_realises" name="articles_realises" value="link"/></td>
                     </tr>
                     <tr>
-                      <td>Les projects réalisés</td>
-                      <td>link</td>
+                        <td><label for="projects_realises">Les projects réalisés</label></td>
+                        <td><input type="text" id="projects_realises" name="projects_realises" value="link"/></td>
                     </tr>
                     <tr>
-                      <td>Autres activités</td>
-                      <td>none</td>
+                        <td><label for="autres_activites">Autres activités</label></td>
+                        <td><input type="text" id="autres_activites" name="autres_activites" value="none"/></td>
                     </tr>
                     <tr>
                       <td>CV</td>
-                      <td>link</td>
+                      <td>
+                      <input className="Post-input" type="file" id="cv" name="cv" />
+                    <label class="ImportImg-cv" for="cv">Upload cv</label>
+                    </td>
                     </tr>
                   </table>
+                  <button class="SaveButton-profil">Save Changes</button>
             </div>
         </div>
     </div>
--- LIASSE/src/components/ProfileProf/index.jsx
+++ LIASSE/src/components/ProfileProf/index.jsx
@@ -11,6 +11,8 @@ function ProfileProf() {
             <div class="A-profil">
                 <div class="profilPic-profil">
                     <img class="img-profil" src={profil} height="120px" width="120px"/>
+                    <input className="Post-input" type="file" id="image" name="image" />
+                    <label class="ImportImg-profil" for="image">Upload Picture</label>
                     <h2>Full Name</h2>
                     <center><hr class="hr-profil"/></center>
                     <table class="T-profil linkTable-profil">
@@ -39,50 +41,54 @@ function ProfileProf() {
             <div class="B-profil">
                 <table class="T-profil ProfileDataTable-profil">
                     <tr>
-                      <td>Nom</td>
-                      <td>Rachid</td>
+                    <td><label for="nom">Nom</label></td>
+                    <td><input type="text" id="nom" name="nom" value="Ali"/></td>
                     </tr>
                     <tr>
-                      <td>Prénom</td>
-                      <td>Rachid</td>
+                        <td><label for="prenom">Prénom</label></td>
+                        <td><input type="text" id="prenom" name="prenom" value="Hamid"/></td>
                     </tr>
                     <tr>
-                      <td>Grade</td>
-                      <td>3</td>
+                        <td><label for="grade">Grade</label></td>
+                        <td><input type="text" id="grade" name="grade" value="2"/></td>
                     </tr>
                     <tr>
-                      <td>Spécialité</td>
-                      <td>Informatique</td>
+                        <td><label for="specialite">Spécialité</label></td>
+                        <td><input type="text" id="specialite" name="specialite" value="Meca"/></td>
                     </tr>
                     <tr>
-                      <td>Thématiques de travail</td>
-                      <td>AI</td>
+                        <td><label for="thematiques">Thématiques de travail</label></td>
+                        <td><input type="text" id="thematiques" name="thematiques" value="Meca"/></td>
                     </tr>
                     <tr>
-                      <td>Les sujects de thèse</td>
-                      <td>link</td>
+                        <td><label for="sujets_these">Les sujects de thèse</label></td>
+                        <td><input type="text" id="sujets_these" name="sujets_these" value="link"/></td>
                     </tr>
                     <tr>
-                      <td>Les articles réalisés</td>
-                      <td>link</td>
+                        <td><label for="articles_realises">Les articles réalisés</label></td>
+                        <td><input type="text" id="articles_realises" name="articles_realises" value="link"/></td>
                     </tr>
                     <tr>
-                      <td>Les doctorants encadrés</td>
-                      <td>link</td>
+                        <td><label for="doctorants_encadres">Les doctorants encadrés</label></td>
+                        <td><input type="text" id="doctorants_encadres" name="doctorants_encadres" value="link"/></td>
                     </tr>
                     <tr>
-                      <td>Les projects réalisés</td>
-                      <td>link</td>
+                        <td><label for="projects_realises">Les projects réalisés</label></td>
+                        <td><input type="text" id="projects_realises" name="projects_realises" value="link"/></td>
                     </tr>
                     <tr>
-                      <td>Autres activités</td>
-                      <td>none</td>
+                        <td><label for="autres_activites">Autres activités</label></td>
+                        <td><input type="text" id="autres_activites" name="autres_activites" value="none"/></td>
                     </tr>
                     <tr>
                       <td>CV</td>
-                      <td>link</td>
+                      <td>
+                      <input className="Post-input" type="file" id="cv" name="cv" />
+                    <label class="ImportImg-cv" for="cv">Upload cv</label>
+                    </td>
                     </tr>
                   </table>
+                  <button class="SaveButton-profil">Save Changes</button>
             </div>
         </div>
     </div>
